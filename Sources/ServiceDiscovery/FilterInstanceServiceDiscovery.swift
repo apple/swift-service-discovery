91034aab5b15877b8b4d3ecea62edb992865a6a5
Cory Benfield <lukasa@apple.com>
2020-06-24 17:01:39 +0100

    Define filterInstance. (#19)
    Motivation:
    
    In apple#17 we added support for transforming instance types for a
    service discovery implementation. This is great, but another common
    requirement might be to exclude some instances altogether based on some
    criterion.
    
    This corresponds directly to another combinator, `filter`. Let's add
    `filterInstance` to allow users to implement this easily.
    
    Modifications:
    
    - Implemented FilterInstanceServiceDiscovery.
    - Added tests
    
    Results:
    
    Users can perform straightforward filtering of instances.

---
+++ Sources/ServiceDiscovery/FilterInstanceServiceDiscovery.swift
@@ -0,0 +1,65 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftServiceDiscovery open source project
+//
+// Copyright (c) 2020 Apple Inc. and the SwiftServiceDiscovery project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftServiceDiscovery project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+import Dispatch
+import ServiceDiscoveryHelpers
+
+public final class FilterInstanceServiceDiscovery<BaseDiscovery: ServiceDiscovery> {
+    typealias Predicate = (BaseDiscovery.Instance) throws -> Bool
+
+    private let originalSD: BaseDiscovery
+    private let predicate: Predicate
+
+    internal init(originalSD: BaseDiscovery, predicate: @escaping Predicate) {
+        self.originalSD = originalSD
+        self.predicate = predicate
+    }
+}
+
+extension FilterInstanceServiceDiscovery: ServiceDiscovery {
+    // This is derived from the base implementation and the transformer.
+    public var instancesToExclude: Set<BaseDiscovery.Instance>? {
+        // We have to crash if the transformer throws here, as we cannot error.
+        self.originalSD.instancesToExclude.map { try! Set($0.filter(self.predicate)) }
+    }
+
+    /// Default timeout for lookup.
+    public var defaultLookupTimeout: DispatchTimeInterval {
+        self.originalSD.defaultLookupTimeout
+    }
+
+    public func lookup(_ service: BaseDiscovery.Service, deadline: DispatchTime?, callback: @escaping (Result<[BaseDiscovery.Instance], Error>) -> Void) {
+        self.originalSD.lookup(service, deadline: deadline) { result in callback(self.transform(result)) }
+    }
+
+    public func subscribe(to service: BaseDiscovery.Service, onNext nextResultHandler: @escaping (Result<[BaseDiscovery.Instance], Error>) -> Void, onComplete completionHandler: @escaping (CompletionReason) -> Void) -> CancellationToken {
+        self.originalSD.subscribe(
+            to: service,
+            onNext: { result in nextResultHandler(self.transform(result)) },
+            onComplete: completionHandler
+        )
+    }
+
+    private func transform(_ result: Result<[BaseDiscovery.Instance], Error>) -> Result<[BaseDiscovery.Instance], Error> {
+        switch result {
+        case .success(let instances):
+            do {
+                return try .success(instances.filter(self.predicate))
+            } catch {
+                return .failure(error)
+            }
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+}
