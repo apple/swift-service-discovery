3dff6880e32df523dd9f0565ff932a2e8749d62b
Yim Lee <yim_lee@apple.com>
2022-07-27 17:44:51 -0700

    Add header

--- Package.swift
+++ Package.swift
@@ -1,4 +1,20 @@
 // swift-tools-version:5.3
+
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftServiceDiscovery open source project
+//
+// Copyright (c) 2020-2022 Apple Inc. and the SwiftServiceDiscovery project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftServiceDiscovery project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+
 import PackageDescription
 
 let package = Package(
