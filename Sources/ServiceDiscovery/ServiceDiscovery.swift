9a2eef460b42bd00374c01c3f6e806bde0949ed6
Yim Lee <yim_lee@apple.com>
2023-01-23 21:17:09 -0800

    Fix Sendable related error in Swift 5.8

--- Sources/ServiceDiscovery/ServiceDiscovery.swift
+++ Sources/ServiceDiscovery/ServiceDiscovery.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftServiceDiscovery open source project
 //
-// Copyright (c) 2019-2022 Apple Inc. and the SwiftServiceDiscovery project authors
+// Copyright (c) 2019-2023 Apple Inc. and the SwiftServiceDiscovery project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -67,7 +67,7 @@ public protocol ServiceDiscovery: AnyObject {
 // MARK: - Subscription
 
 /// Enables cancellation of service discovery subscription.
-public class CancellationToken {
+public class CancellationToken: @unchecked Sendable {
     private let _isCancelled: ManagedAtomic<Bool>
     private let _completionHandler: (CompletionReason) -> Void
 
